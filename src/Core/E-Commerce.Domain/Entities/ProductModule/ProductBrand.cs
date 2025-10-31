d41f6c353a27f562fee5cc3ddc54ab662eb06ce0
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-10-31 19:17:06 +0200

    Product Module Entities

---
+++ src/Core/E-Commerce.Domain/Entities/ProductModule/ProductBrand.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce.Domain.Entities.ProductModule
+{
+    public class ProductBrand : BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
