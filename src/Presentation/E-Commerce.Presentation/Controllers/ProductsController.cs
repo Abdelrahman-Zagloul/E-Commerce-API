63a2a045a2e033605fa7b04fbc4ed0096ae3d55c
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-10-31 19:51:58 +0200

    Product Controller

---
+++ src/Presentation/E-Commerce.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using E_Commerce.ServicesAbstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("brand")]
+        public async Task<IActionResult> GetAllBrands()
+        {
+            var brands = await _productService.GetAllBrandsAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("type")]
+        public async Task<IActionResult> GetAllTypes()
+        {
+            var types = await _productService.GetAllTypesAsync();
+            return Ok(types);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getProductById(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+    }
+}
