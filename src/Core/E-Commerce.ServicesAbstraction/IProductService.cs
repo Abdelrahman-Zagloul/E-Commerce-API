685bc443e584f9a685e33d848708f852a5bf6841
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-10-31 19:48:08 +0200

    Product Service

---
+++ src/Core/E-Commerce.ServicesAbstraction/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using E_Commerce.Shared.DTOs.Products;
+
+namespace E_Commerce.ServicesAbstraction
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<IEnumerable<ProductBrandDto>> GetAllBrandsAsync();
+        Task<IEnumerable<ProductTypeDto>> GetAllTypesAsync();
+        Task<ProductDto> GetProductByIdAsync(int id);
+    }
+}
---
+++ src/E-Commerce.Shared/DTOs/Products/ProductTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce.Shared.DTOs.Products
+{
+    public class ProductTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+}
