5a4a6867e8741857fe3e641dae795260b80d51bf
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-10-31 19:41:05 +0200

    Generic Repository & Unit Of Work

---
+++ src/Core/E-Commerce.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Domain.Entities;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<TEntity, TKey> Repository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+        Task<int> SaveChangesAsync();
+    }
+}
