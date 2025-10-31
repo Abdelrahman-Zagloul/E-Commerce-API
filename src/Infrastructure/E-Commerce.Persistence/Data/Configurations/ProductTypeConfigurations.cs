09dd5691bb35a85eb77ebcaa73998702369e26b8
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-10-31 19:25:28 +0200

    Add Configuration & Context & Migrations

---
+++ src/Infrastructure/E-Commerce.Persistence/Data/Configurations/ProductTypeConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Domain.Entities.ProductModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace E_Commerce.Persistence.Data.Configurations
+{
+    internal class ProductTypeConfigurations : IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.ToTable("ProductTypes");
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/Infrastructure/E-Commerce.Persistence/Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.Domain.Entities.ProductModule;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Persistence.Data.Contexts
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(IPersistenceAssemblyMarker).Assembly);
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
