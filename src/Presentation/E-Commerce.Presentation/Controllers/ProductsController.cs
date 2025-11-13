a5890cdd16012d682cad1e02168f524c6ad3f622
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-11-13 22:36:30 +0200

    Cashe Attribute

--- src/Presentation/E-Commerce.Presentation/Controllers/ProductsController.cs
+++ src/Presentation/E-Commerce.Presentation/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using E_Commerce.ServicesAbstraction;
+﻿using E_Commerce.Presentation.Attributes;
+using E_Commerce.ServicesAbstraction;
 using E_Commerce.Shared.Parameters;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +18,7 @@ namespace E_Commerce.Presentation.Controllers
         }
 
         [HttpGet]
+        [RedisCashe(10)]
         public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParams queryParams)
         {
             var products = await _productService.GetAllProductsAsync(queryParams);
@@ -24,6 +26,7 @@ namespace E_Commerce.Presentation.Controllers
         }
 
         [HttpGet("brand")]
+        [RedisCashe(10)]
         public async Task<IActionResult> GetAllBrands()
         {
             var brands = await _productService.GetAllBrandsAsync();
@@ -31,13 +34,15 @@ namespace E_Commerce.Presentation.Controllers
         }
 
         [HttpGet("type")]
+        [RedisCashe(10)]
         public async Task<IActionResult> GetAllTypes()
         {
             var types = await _productService.GetAllTypesAsync();
             return Ok(types);
         }
+
         [HttpGet("{id}")]
-        public async Task<IActionResult> getProductById(int id)
+        public async Task<IActionResult> GetProductById(int id)
         {
             var product = await _productService.GetProductByIdAsync(id);
             return Ok(product);
