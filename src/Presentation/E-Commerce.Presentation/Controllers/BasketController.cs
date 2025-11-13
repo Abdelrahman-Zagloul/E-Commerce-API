b62b76794cd6ba4a8a38ce9e5d80e53626cae498
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-11-13 16:52:49 +0200

    Basket Controller

---
+++ src/Presentation/E-Commerce.Presentation/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using E_Commerce.ServicesAbstraction;
+using E_Commerce.Shared.DTOs.Baskets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketController : ControllerBase
+    {
+
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BasketDto>> GetBasket(string id)
+        {
+            var result = await _basketService.GetBasketAsync(id);
+            if (result is null)
+                return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BasketDto>> AddOrUpdate([FromBody] BasketDto basketDto)
+        {
+            var result = await _basketService.CreateOrUpdateBasketAsync(basketDto);
+            return Ok(result);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBasket(string id)
+        {
+            await _basketService.DeleteBasketAsync(id);
+            return NoContent();
+        }
+    }
+}
