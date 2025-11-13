1d3c3a2e95175c37efad5cb0d2dd605853f85081
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-11-13 15:23:44 +0200

    Basket Repository

---
+++ src/Core/E-Commerce.Domain/Contracts/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Domain.Entities.BasketModule;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> CreateOrUpdateBasketAsync(CustomerBasket basket, TimeSpan timeToLive);
+        Task<CustomerBasket?> GetBasketAsync(string basketId);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
