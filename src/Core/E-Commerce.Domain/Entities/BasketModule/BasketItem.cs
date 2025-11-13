6a0b224bbad324b0a33d167dcb5a1d0b9f0771cb
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-11-13 14:24:36 +0200

    Basket Module

---
+++ src/Core/E-Commerce.Domain/Entities/BasketModule/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Domain.Entities.BasketModule
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
