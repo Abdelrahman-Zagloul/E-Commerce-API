8e2e3f844d299bf021f2970cb35b6f18a70c02c3
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-11-19 11:09:16 +0200

    Add IdentityContext & Migration

---
+++ src/Infrastructure/E-Commerce.Persistence/IdentityData/Contexts/StoreIdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Domain.Entities.IdentityModule;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Persistence.IdentityData.Contexts
+{
+    public class StoreIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public StoreIdentityDbContext(DbContextOptions<StoreIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Address>().ToTable("Addresses");
+        }
+    }
+}
