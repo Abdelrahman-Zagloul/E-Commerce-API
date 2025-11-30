2d44d049fa9d2eec4e345b1282a091310939c7b0
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-11-30 10:57:44 +0200

    Order Entities

---
+++ src/Core/E-Commerce.Domain/Entities/OrderModule/DeliveryMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Domain.Entities.OrderModule
+{
+    public class DeliveryMethod : BaseEntity<int>
+    {
+        public string ShortName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string DeliveryTime { get; set; } = default!;
+        public decimal Cost { get; set; }
+        public ICollection<Order> Orders { get; set; } = [];
+    }
+}
