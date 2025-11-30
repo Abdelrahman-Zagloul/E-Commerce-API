1d229a831cfd4f65b610d77fa808aba3beb4e46b
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-11-30 10:57:44 +0200

    Order Entities

---
+++ src/Core/E-Commerce.Domain/Entities/OrderModule/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace E_Commerce.Domain.Entities.OrderModule
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderStatus status { get; set; } = OrderStatus.Pending;
+        public OrderAddress ShippingAddress { get; set; } = default!; // Owned Entity
+        public decimal SubTotal { get; set; }
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Cost;
+
+
+        public int DeliveryMethodId { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+        public ICollection<OrderItem> Items { get; set; } = [];
+
+    }
+}
---
+++ src/Core/E-Commerce.Domain/Entities/OrderModule/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce.Domain.Entities.OrderModule
+{
+    public enum OrderStatus
+    {
+        Pending,
+        PaymentReceived,
+        PaymentFailed,
+    }
+}
