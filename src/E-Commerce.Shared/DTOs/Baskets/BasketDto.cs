515ddb38088dbef46a027920f612dbde8c52e3c3
Abdelrahman Zagloul <abdelrahman.zagloul.dev@gmail.com>
2025-12-13 14:06:51 +0200

    Fix: Update BasketDto

--- src/E-Commerce.Shared/DTOs/Baskets/BasketDto.cs
+++ src/E-Commerce.Shared/DTOs/Baskets/BasketDto.cs
@@ -1,11 +1,12 @@
 ﻿namespace E_Commerce.Shared.DTOs.Baskets
 {
-    public record BasketDto(
-        string Id,
-        ICollection<BasketItemDto> Items,
-        string? PaymentIntentId,
-        string? ClientSecret,
-        int? DeliveryMethodId,
-        decimal ShippingPrice
-    );
+    public class BasketDto
+    {
+        public string Id { get; init; } = default!;
+        public ICollection<BasketItemDto> Items { get; init; } = default!;
+        public string? PaymentIntentId { get; init; }
+        public string? ClientSecret { get; init; }
+        public int? DeliveryMethodId { get; init; }
+        public decimal ShippingPrice { get; init; }
+    }
 }
